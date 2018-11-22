894459d95db00bbb7304107517f54fd8cee277bc
Eftimie Andrei <bax@baxandrei.ro>
2018-11-22 13:12:53 +0200

    Create forms.php

---
+++ upload/modules/Forms/language/Romanian/forms.php
@@ -0,0 +1,61 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/partydragen/Nameless-Forms
+ *  NamelessMC version 2.0.0-pr4
+ *
+ *  License: MIT
+ *
+ *  Romanian Language for Forms module
+ *  Translation By @BaxAndrei ( https://baxandrei.ro )
+ *  Last Update: 22/11/2018
+ */
+
+$language = array(
+	// Forms
+	'forms' => 'Formulare',
+	'form' => 'Formular',
+	'new_form' => 'Formular nou',
+	'form_name' => 'Numele formularului',
+	'form_url' => 'Adresa URL pentru formular (cu / înainte, de exemplu: /exemplu)',
+	'form_icon' => 'Iconiță formular',
+	'link_location' => 'Locația legăturii',
+	'allow_guests' => 'Vizitatorii pot vizita acest formular?',
+	'creating_new_form' => 'Creați un formular nou',
+	'form_created_successfully' => 'Formularul a fost creat cu succes',
+	'none_forms_defined' => 'Nu există încă formulare create.',
+	'confirm_delete_form' => 'Sigur doriți să ștergeți acest formular? Toate câmpurile pentru acest formular vor fi șterse.',
+	
+	// Fields
+	'field' => 'Câmp',
+	'fields' => 'Câmpuri',
+	'new_field' => 'Câmp nou',
+	'field_name' => 'Numele câmpului',
+	'field_created_successfully' => 'Câmpul a fost creat cu succes',
+	'none_fields_defined' => 'Nu există încă câmpuri create.',
+	'confirm_delete_field' => 'Sigur doriți să ștergeți acest câmp?',
+	
+	// Field types
+	'field_type' => 'Tipul câmpului',
+	'text' => 'Text',
+	'options' => 'Opțiuni',
+	'textarea' => 'Zonă de text',
+	
+	'required' => 'Necesar',
+	'options_help' => 'Fiecare opțiune pe o nouă linie; foate fi lăsat gol (numai pentru opțiuni)',
+	'cancel' => 'Anulare',
+	
+	// AdminCP Errors
+	'input_form_name' => 'Introduceți un nume pentru formular.',
+	'input_form_url' => 'Introduceți o adresă URL pentru formular.',
+	'form_name_minimum' => 'Numele formularului trebuie să aibă cel puțin 2 caractere.',
+	'form_url_minimum' => 'Adresa URL a formularului trebuie să aibă cel puțin 2 caractere.',
+	'form_name_maximum' => 'Numele formularului trebuie să aibă maxim 32 de caractere.',
+	'form_url_maximum' => 'Adresa URL a formularului trebuie să aibă maximum 32 de caractere.',
+	'form_icon_maximum' => 'Iconița pentru formular trebuie să aibă maximum 64 de caractere.',
+	'input_field_name' => 'Introduceți un nume pentru câmp.',
+	'field_name_minimum' => 'Numele câmpului trebuie să aibă cel puțin 2 caractere.',
+	'field_name_maximum' => 'Numele câmpului trebuie să aibă maximum 255 de caractere.',
+	
+	
+);
