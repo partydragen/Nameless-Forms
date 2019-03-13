3855a16e7a82292be1b5712d3ac9e112d0927b96
Hyfalls <saker.web1@gmail.com>
2019-03-13 22:37:11 +0100

    Swedish language

---
+++ upload/modules/Forms/language/SwedishSE/forms.php
@@ -0,0 +1,88 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/partydragen/Nameless-Forms
+ *  NamelessMC version 2.0.0-pr4
+ *
+ *  License: MIT
+ *
+ *  SwedishSE Language for Forms module by Hyfalls
+ */
+
+$language = array(
+	// Forms
+	'forms' => 'Formulär',
+	'form' => 'Formulär',
+	'new_form' => 'Nytt formulär',
+	'form_name' => 'Formulärets Rubrik',
+	'form_url' => 'Formulärets URL (med föregående /, t.ex /exempel)',
+	'form_icon' => 'Formulärets Ikon',
+	'link_location' => 'Länk Plats',
+	'allow_guests' => 'Kan gäster visa detta formulär?',
+	'creating_new_form' => 'Skapar nytt formulär',
+	'none_forms_defined' => 'Det finns inga formulär än.',
+	'delete_form' => 'Är du säker på att du vill radera det här formuläret?</br>Varning: Varning: Alla uppgifter som hör till detta formulär raderas (frågor och inlägg)',
+	'form_submitted' => 'Formulär skickad',
+	'action' => 'Action',
+	
+	// Form
+	'editing_x' => 'Redigerar {x}', // Don't replace {x}
+	'form_created_successfully' => 'Formuläret har skapats',
+	'form_updated_successfully' => 'Formuläret har uppdaterats.',
+	'form_deleted_successfully' => 'Formuläret har raderats.',
+	
+	// Fields
+	'field' => 'Fält',
+	'fields' => 'Fält',
+	'new_field' => 'Nytt Fält',
+	'field_name' => 'Fält namn',
+	'field_created_successfully' => 'Fältet har skapats',
+	'field_updated_successfully' => 'Fältet har uppdaterats',
+	'field_deleted_successfully' => 'Fältet har raderats',
+	'new_field_for_x' => 'Skapar nytt fält för {x}',
+	'editing_field_for_x' => 'Redigerar fält för{x}',
+	'none_fields_defined' => 'Det finns inga fält än',
+	'confirm_delete_field' => 'Är du säker på att du vill radera detta fält?',
+	'options' => 'Inställningar',
+	'options_help' => 'Varje alternativ på en ny linje; kan lämnas tomt (endast alternativ)',
+	'field_order' => 'Fältorder',
+	'delete_field' => 'Är du säker på att du vill radera detta fält?',
+	
+	// Statuses
+	'statuses' => 'statusar',
+	'status' => 'Status',
+	'new_status' => 'Ny Status',
+	'creating_status' => 'Skapar ny status',
+	'editing_status' => 'Redigerar status',
+	'marked_as_open' => 'Markerad som öppen',
+	'status_name' => 'Status Namn',
+	'status_html' => 'Status HTML',
+	'status_forms' => 'Status Formulär',
+	'status_groups' => 'Status Grupper',
+	'status_creation_success' => 'Status har skapats.',
+	'status_creation_error' => 'Ett fel uppstod vid att skapa en status. Se till att statusets html inte är längre än 1024 tecken.',
+	'status_edit_success' => 'Status har uppdaterats.',
+	'status_deleted_successfully' => 'Status deleted successfully.',
+	'delete_status' => 'Är du säker på att du vill radera detta status?',
+
+	// Errors
+	'input_form_name' => 'Vänligen ange ett formulär rubrik.',
+	'input_form_url' => 'Vänligen ange ett formulär länk.',
+	'form_name_minimum' => 'Formulärets rubrik måste vara minst 2 tecken.',
+	'form_url_minimum' => 'Formulärets länk måste vara minst 2 tecken.',
+	'form_name_maximum' => 'Formulärets rubrik måste vara högst 32 tecken.',
+	'form_url_maximum' => 'Formulärets länk måste vara högst 32 tecken.',
+	'form_icon_maximum' => 'Formulärets ikon måste vara högst 64 tecken.',
+	'input_field_name' => 'Vänligen ange ett fält namn.',
+	'field_name_minimum' => 'Fält namnet måste vara minst 2 tecken.',
+	'field_name_maximum' => 'Fält namnet måste vara högst 255 tecken.',
+	
+	// Submissions
+	'submissions' => 'Inskickade',
+	'no_open_submissions' => 'Det finns inga öppna inskickningar just nu.',
+	'no_closed_submissions' => 'Det finns inga stängda inskickningar just nu.',
+	'form_x' => 'Formulär: {x}',
+	'current_status_x' => 'Nuvarande Status: {x}',
+	'last_updated' => 'Sist uppdaterad:',
+	'guest' => 'Gäst',
+);
