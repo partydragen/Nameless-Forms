2a5aa81234ecd4b10916ef007e915423ce62f2a1
Fjuro <40403606+Fjuro@users.noreply.github.com>
2021-01-09 14:44:26 +0100

    Create forms.php

---
+++ upload/modules/Forms/language/Czech/forms.php
@@ -0,0 +1,120 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/partydragen/Nameless-Forms
+ *  https://partydragen.com/
+ *  NamelessMC version 2.0.0-pr8
+ *
+ *  License: MIT
+ *
+ *  Czech Language for Forms module
+ */
+
+$language = array(
+	// Forms
+	'forms' => 'Formuláře',
+	'form' => 'Formulář',
+	'new_form' => 'Nový formulář',
+	'form_name' => 'Název formuláře',
+	'form_url' => 'URL formuláře (s předchozím /, např. /priklad)',
+	'form_icon' => 'Ikona formuláře',
+	'link_location' => 'Umístění odkazu',
+	'creating_new_form' => 'Vytváření nového formuláře',
+	'form_created_successfully' => 'Formulář úspěšně vytvořen',
+	'none_forms_defined' => 'Zatím jste nevytvořili žádné formuláře.',
+	'delete_form' => 'Opravdu chcete odstranit tento formulář?</br>Varování: Všechna data související s tímto formulářem, jako otázky a odpovědi, budou smazány',
+	'form_submitted' => 'Formulář úspěšně odeslán',
+	'action' => 'Akce',
+	'actions' => 'Akce',
+	'guest' => 'Host',
+	
+	// Permissions
+	'forms_view_submissions' => 'StaffCP &raquo; Formuláře &raquo; Odpovědi',
+	'forms_manage' => 'StaffCP &raquo; Formuláře &raquo; Formuláře',
+	
+	// Form
+	'editing_x' => 'Úprava formuláře {x}', // Don't replace {x}
+	'form_created_successfully' => 'Formulář úspěšně vytvořen.',
+	'form_updated_successfully' => 'Formulář úspěšně upraven.',
+	'form_deleted_successfully' => 'Formulář úspěšně odstraněn.',
+	'allow_guests' => 'Mohou si hosti zobrazit tento formulář?',
+	'allow_guests_help' => 'Hosti budou moct odesílat odpovědi bez přihlášení. Po odeslání si ale nebudou moci své odpovědi zobrazit',
+	'can_user_view' => 'Může si uživatel zobrazit své vlastní odpovědi?',
+	'can_user_view_help' => 'Uživatel si bude moct zobrazit jeho vlastní odpovědi a použít sekci komentářů. Bude také dostávat upozornění o aktualizacích při změně stavu nebo když jeho odpovědi někdo okomentuje. Toto nebude fungovat pro hosty.',
+	
+	// Fields
+	'field' => 'Pole',
+	'fields' => 'Pole',
+	'new_field' => 'Nové pole',
+	'field_name' => 'Název pole',
+	'field_created_successfully' => 'Pole úspěšně vytvořeno',
+	'field_updated_successfully' => 'Pole úspěšně upraveno',
+	'field_deleted_successfully' => 'Pole úspěšně odstraněno',
+	'new_field_for_x' => 'Vytváření nového pole ve formuláři {x}',
+	'editing_field_for_x' => 'Úprava pole ve formuláři {x}',
+	'none_fields_defined' => 'Zatím jste nevytvořili žádná pole.',
+	'confirm_delete_field' => 'Opravdu chcete odstranit toto pole?',
+	'options' => 'Možnosti',
+	'options_help' => 'Každá možnost na nový řádek; může být ponecháno prázdné (pouze možnosti). Do tohoto boxu byste také měli zadat text nápovědy.',
+	'field_order' => 'Pořadí pole',
+	'delete_field' => 'Opravdu chcete odstranit toto pole?',
+	'help_box' => 'Text nápovědy',
+	'barrier' => 'Oddělovač',
+	'number' => 'Číslo',
+
+	// Statuses
+	'statuses' => 'Stavy',
+	'status' => 'Stav',
+	'new_status' => 'Nový stav',
+	'creating_status' => 'Vytváření nového stavu',
+	'editing_status' => 'Úprava stavu',
+	'marked_as_open' => 'Označeno jako otevřené',
+	'status_name' => 'Název stavu',
+	'status_html' => 'HTML stavu',
+	'status_forms' => 'Formuláře stavu',
+	'status_groups' => 'Skupiny stavu',
+	'status_creation_success' => 'Status úspěšně vytvořen.',
+	'status_creation_error' => 'Při vytváření stavu se vyskytla chyba. Ujistěte se, že HTML stavu není delší než 1024 znaků.',
+	'status_edit_success' => 'Stav úspěšně upraven.',
+	'status_deleted_successfully' => 'Stav úspěšně odstraněn.',
+	'delete_status' => 'Opravdu chcete odstranit tento stav?',
+
+	// Errors
+	'input_form_name' => 'Zadejte prosím název formuláře.',
+	'input_form_url' => 'Zadejte prosím URL formuláře.',
+	'form_name_minimum' => 'Název formuláře musí obsahovat minimálně 2 znaky.',
+	'form_url_minimum' => 'URL formuláře musí obsahovat minimálně 2 znaky.',
+	'form_name_maximum' => 'Název formuláře může obsahovat maximálně 32 znaků.',
+	'form_url_maximum' => 'URL formuláře může obsahovat maximálně 32 znaků.',
+	'form_icon_maximum' => 'Ikona formuláře může obsahovat maximálně 64 znaků.',
+	'input_field_name' => 'Zadejte prosím název pole.',
+	'field_name_minimum' => 'Název pole musí obsahovat minimálně 2 znaky.',
+	'field_name_maximum' => 'Název pole může obsahovat maximálně 255 znaků.',
+	
+	// Submissions
+	'submissions' => 'Odpovědi',
+	'submission_updated' => 'Odpověď úspěšně upravena',
+	'no_open_submissions' => 'Momentálně zde nejsou žádné otevřené odpovědi.',
+	'no_closed_submissions' => 'Momentálně zde nejsou žádné uzavřené odpovědi.',
+	'form_x' => 'Formulář: {x}',
+	'current_status_x' => 'Status: {x}',
+	'last_updated' => 'Naposledy aktualizováno:',
+	'your_submission_updated' => 'Vaše odpověď byla upravena',
+	'user' => 'Uživatel',
+	'updated_by' => 'Upravil',
+	'sort' => 'Pořadí',
+	
+	// Update alerts
+	'new_update_available_x' => 'Je dostupná nová aktualizace doplňku {x}',
+	'new_urgent_update_available_x' => 'Je dostupná nová závažná aktualizace doplňku {x}. Aktualizujte jak nejdříve je to možné!',
+	'current_version_x' => 'Současná verze doplňku: {x}',
+	'new_version_x' => 'Nová verze doplňku: {x}',
+	'view_resource' => 'Zobrazit stránku doplňku',
+	
+	// Hook
+    'new_form_submission' => 'Nová odpověď ve formuláři',
+    'updated_form_submission' => 'Nový komentář u odpovědi formuláře',
+    'new_submission_text' => 'Nová odpověď vytvořena u formuláře {x} uživatelem {y}',
+    'updated_submission_text' => 'Komentář nové odpovědi u formuláře {x} od uživatele {y}'
+    //'updated_submission_text' => 'Odpověď u formuláře {x} aktualizována uživatelem {y}'
+);
