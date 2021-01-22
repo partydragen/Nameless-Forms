0ab6af4bc3b2e5baafc9538fe9c006510324297f
Partydragen <admin@partydragen.com>
2021-01-22 19:23:30 +0100

    fix error notice in update checker

--- upload/modules/Forms/module.php
+++ upload/modules/Forms/module.php
@@ -45,7 +45,10 @@ class Forms_Module extends Module {
             if($module_version != $cache->retrieve('module_version')) {
                 // Version have changed, Perform actions
                 $cache->store('module_version', $module_version);
-                $cache->erase('update_check');
+                
+                if($cache->isCached('update_check')){
+                    $cache->erase('update_check');
+                }
             }
         }
         
