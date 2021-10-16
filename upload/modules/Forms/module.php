eb2961d98cb307242d5ded5f9b9b6d8f8529776b
Partydragen <admin@partydragen.com>
2021-10-16 20:22:48 +0200

    Release 1.8.1

--- upload/modules/Forms/module.php
+++ upload/modules/Forms/module.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Partydragen
  *  https://github.com/partydragen/Nameless-Forms
- *  NamelessMC version 2.0.0-pr11
+ *  NamelessMC version 2.0.0-pr12
  *
  *  License: MIT
  *
@@ -21,8 +21,8 @@ class Forms_Module extends Module {
 
         $name = 'Forms';
         $author = '<a href="https://partydragen.com" target="_blank" rel="nofollow noopener">Partydragen</a>';
-        $module_version = '1.8.0';
-        $nameless_version = '2.0.0-pr11';
+        $module_version = '1.8.1';
+        $nameless_version = '2.0.0-pr12';
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
         
