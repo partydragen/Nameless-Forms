b95e67a3516db4c796e2944b882a47d6e910bee4
Partydragen <admin@partydragen.com>
2022-06-25 20:02:12 +0200

    Release v1.9.1

--- upload/modules/Forms/module.php
+++ upload/modules/Forms/module.php
@@ -23,7 +23,7 @@ class Forms_Module extends Module {
 
         $name = 'Forms';
         $author = '<a href="https://partydragen.com" target="_blank" rel="nofollow noopener">Partydragen</a>';
-        $module_version = '1.9.0';
+        $module_version = '1.9.1';
         $nameless_version = '2.0.0-pr13';
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
